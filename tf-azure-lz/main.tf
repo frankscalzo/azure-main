7f8741b8fc3f4bc367605b206ff76843aa375cf5
Frank X Scalzo <fscalzo@gmail.com>
2022-08-24 14:22:43 -0400

    fixing things

---
+++ tf-azure-lz/main.tf
@@ -0,0 +1,41 @@
+# We strongly recommend using the required_providers block to set the
+# Azure Provider source and version being used.
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.0.2"
+    }
+  }
+}
+
+provider "azurerm" {
+  tenant_id              = "7f01327e-9d29-46bb-915d-c67edf4dcf4e"
+  subscription_id = "07f192e5-05e9-4c31-91a4-eabbd2be9099"
+  features {}
+}
+
+# You can use the azurerm_client_config data resource to dynamically
+# extract connection settings from the provider configuration.
+
+data "azurerm_client_config" "core" {}
+
+# Call the caf-enterprise-scale module directly from the Terraform Registry
+# pinning to the latest version
+
+module "enterprise_scale" {
+  source  = "Azure/caf-enterprise-scale/azurerm"
+  version = "2.3.1"
+
+  providers = {
+    azurerm              = azurerm
+    azurerm.connectivity = azurerm
+    azurerm.management   = azurerm
+  }
+
+  root_parent_id = data.azurerm_client_config.core.tenant_id
+  root_id        = "ohit-demo"
+  root_name      = "OHIT Demo Organization"
+
+}
